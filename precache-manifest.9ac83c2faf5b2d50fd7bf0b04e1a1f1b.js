62460b6c3b9dc09517871a42fd854de48418e003
gonzalopimentel <gonzalo.pimentel.ramirez@gmail.com>
2020-07-31 10:27:25 -0500

    Updates

---
+++ precache-manifest.9ac83c2faf5b2d50fd7bf0b04e1a1f1b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "f67eb99f7f9f3ae9d606",
+    "url": "/Badge-Management-System/static/css/main.b35f671e.chunk.css"
+  },
+  {
+    "revision": "f67eb99f7f9f3ae9d606",
+    "url": "/Badge-Management-System/static/js/main.f67eb99f.chunk.js"
+  },
+  {
+    "revision": "bb89ab0f8eba9d8da40f",
+    "url": "/Badge-Management-System/static/js/runtime~main.bb89ab0f.js"
+  },
+  {
+    "revision": "005d298407c071c50b76",
+    "url": "/Badge-Management-System/static/css/2.119e998a.chunk.css"
+  },
+  {
+    "revision": "005d298407c071c50b76",
+    "url": "/Badge-Management-System/static/js/2.005d2984.chunk.js"
+  },
+  {
+    "revision": "f246f5f1fb702016606ee8c65e33c029",
+    "url": "/Badge-Management-System/static/media/platziconf-logo.f246f5f1.svg"
+  },
+  {
+    "revision": "973f5842f0c37eacb2eb491c62fc0032",
+    "url": "/Badge-Management-System/static/media/badge-header.973f5842.svg"
+  },
+  {
+    "revision": "0026b862f1b05c3e27d471ba85c6ebc5",
+    "url": "/Badge-Management-System/static/media/logo.0026b862.svg"
+  },
+  {
+    "revision": "71addf454967249472471f5398055e3d",
+    "url": "/Badge-Management-System/static/media/astronauts.71addf45.svg"
+  },
+  {
+    "revision": "1d17360a685ac86023b90aa14fd2c62d",
+    "url": "/Badge-Management-System/static/media/stars.1d17360a.svg"
+  },
+  {
+    "revision": "240c85972bc600c6c1c1f8f8585c240a",
+    "url": "/Badge-Management-System/index.html"
+  }
+];
